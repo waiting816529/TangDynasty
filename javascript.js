4fb6c73e99b5225a9780ac5164d811e5f3cdf7f9
waiting816529 <64601034+waiting816529@users.noreply.github.com>
2021-12-23 14:48:57 +0800

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,78 @@
+let cloud=document.getElementById('cloud');
+let decoration=document.getElementById('decoration');
+let title=document.getElementById('title');
+let rfa=document.getElementById('rfa');
+let lfa=document.getElementById('lfa');
+let btn=document.getElementById('btn');
+// let flower=document.getElementById('flower');
+let deco=document.getElementById('intro_deco');
+
+window.addEventListener('scroll',function(){
+    let value=window.scrollY;
+    // flower.style.opacity=0.3;
+    cloud.style.top=value*-0.3+'px';
+    decoration.style.top=value*-0.1+'px';
+    rfa.style.left=value*0.8+'px';
+    lfa.style.left=value*-0.8+'px';
+    title.style.top=value*-0.1+'px';
+    title.style.opacity=(0-scrolled) / 50;/*到底要怎樣才會變透明啦吼吼*/
+});
+
+window.onload = function() {
+    if(document.body.scrollTop > 0) {
+        console.log(1);
+        window.scrollTo(0, -1);
+        document.body.scrollTop = 0;
+    }
+    window.scrollTo(0, -1);
+    document.body.scrollTop = 0;
+}
+
+
+$(window).on("load", function () {
+    $(".loading_wrapper").fadeOut("slow");
+});
+
+$('.card1').hover(function(){
+    if($(this).hasClass("active")){
+        $('.card1 .waiting_task').slideUp(function(){
+            $('.card1').removeClass("active");
+        });
+    }else{
+        $('.card1').addClass("active");
+        $('.card1 .waiting_task').stop().slideDown();
+    }
+});
+
+$('.card2').hover(function(){
+    if($(this).hasClass("active")){
+        $('.card2 .jiayu_task').slideUp(function(){
+            $('.card2').removeClass("active");
+        });
+    }else{
+        $('.card2').addClass("active");
+        $('.card2 .jiayu_task').stop().slideDown();
+    }
+});
+
+$('.card3').hover(function(){
+    if($(this).hasClass("active")){
+        $('.card3 .xioching_task').slideUp(function(){
+            $('.card3').removeClass("active");
+        });
+    }else{
+        $('.card3').addClass("active");
+        $('.card3 .xioching_task').stop().slideDown();
+    }
+});
+
+$('.card4').hover(function(){
+    if($(this).hasClass("active")){
+        $('.card4 .jingwen_task').slideUp(function(){
+            $('.card4').removeClass("active");
+        });
+    }else{
+        $('.card4').addClass("active");
+        $('.card4 .jingwen_task').stop().slideDown();
+    }
+});
